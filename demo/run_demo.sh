e6f0a00b1259ec00deed02f59f18bba52632af13
Joanna Krupka <jak75@ws-s-53.data.cluster>
2025-05-15 14:38:33 +0100

    Minor bug fixes with atrificial data

--- demo/run_demo.sh
+++ demo/run_demo.sh
@@ -5,8 +5,10 @@ nice snakemake \
   --conda-frontend conda \
   -s Snakemake_demo.smk \
   --jobs 1 \
-  --cluster-cancel scancel \
-  --cluster 'sbatch --mem={resources.mem_mb} --ntasks {threads} --partition={resources.slurm_partition} --time={resources.runtime}' \
-  --default-resources slurm_partition="icelake" slurm_account="hodson-sl3-cpu" runtime=60 \
-  --slurm \
   aule_test
+
+#### Consider adding these lines for HPC tests
+ # --cluster-cancel scancel \
+ #  --cluster 'sbatch --mem={resources.mem_mb} --ntasks {threads} --partition={resources.slurm_partition} --time={resources.runtime}' \
+ #  --default-resources slurm_partition="icelake" slurm_account="hodson-sl3-cpu" runtime=60 \
+ #  --slurm \
